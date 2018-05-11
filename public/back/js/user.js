37bbc9d8e4e6280b527e91a23d7cda6ee7f10745
Clark <Clarkabvc@sina.com>
2018-05-11 21:30:36 +0800

    用户管理禁用功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -32,5 +32,39 @@ $(function () {
     })
   }
   
+  //点击设置用户状态,因为是动态渲染的,所以要用事件委托
+  var userId = 0;  //记录点击的用户id
+  var isDelete = 0; //记录用户状态
+  
+  $('tbody').on('click', '.btn', function () {
+    $('#userModal').modal('show');   //显示模态框
+    userId = $(this).parent().data('id'); //获取用户id
+    
+    //判断用户状态, 1 正常  0  禁用
+    // 根据当前按钮的类名,有success的代表禁用,点击需要启用 就设置1  反之设置0
+    
+    isDelete = $(this).hasClass("btn-success") ? 1 : 0;
+    
+  });
+  
+  
+  //点击模态框的确定按钮,设置用户
+  $('#submitBtn').on('click', function () {
+    //发送请求到后台,修改用户状态,并且重新渲染页面
+    
+   
+    $.ajax({
+      type: "post",
+      url: "/user/updateUser",
+      data: {
+        id: userId,
+        isDelete: isDelete
+      },
+      success: function (info) {
+        $('#userModal').modal('hide');
+        render();
+      }
+    })
+  })
   
 });
